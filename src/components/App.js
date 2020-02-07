57c03cbae1ee0e55bf60946c0bfcbf02c6b55cc9
Thong Do <tdo05824@gmail.com>
2020-02-08 00:16:36 +0700

    first time adding provider to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import UserCreate from "./userCreate";
+import languageContext from "../contexts/languageContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -22,7 +23,9 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("vietnamese")}
           />
         </div>
-        <UserCreate />
+        <languageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </languageContext.Provider>
       </div>
     );
   }
