8f0ea931b9fe7bc3c99e494fa2babe58cd0b592b
Thong Do <tdo05824@gmail.com>
2020-02-07 19:05:59 +0700

    create field, button, and userCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import UserCreate from "./userCreate";
 
-const App = () => {
-  return <div className="ui container">App</div>;
-};
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag vn"
+            onClick={() => this.onLanguageChange("vietnamese")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
 
 export default App;
